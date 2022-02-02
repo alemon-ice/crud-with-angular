d9c3f6f90ebb36c50daf25a0f4452d9c6a5cf585
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2022-02-02 19:22:52 -0300

    error handler

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Observable } from 'rxjs';
+import { catchError, EMPTY, map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Product } from './product.model';
 
@@ -13,31 +13,53 @@ export class ProductService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
-  showMessage(msg: string) {
-    this.snackBar.open(msg, 'x', {
+  showMessage(msg: string, isError: boolean = false) {
+    this.snackBar.open(msg, undefined, {
       duration: 3000,
       verticalPosition: 'top',
       horizontalPosition: 'right',
+      panelClass: isError ? 'msg-error' : 'msg-success',
     });
   }
 
+  errorHandler(err: any, message = 'Ocorreu um erro'): Observable<any> {
+    this.showMessage(message, true);
+
+    return EMPTY;
+  }
+
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.apiURL, product);
+    return this.http.post<Product>(this.apiURL, product).pipe(
+      map((obj) => obj),
+      catchError((err) => this.errorHandler(err, 'Erro ao criar produto'))
+    );
   }
 
   readAll(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.apiURL);
+    return this.http.get<Product[]>(this.apiURL).pipe(
+      map((obj) => obj),
+      catchError((err) => this.errorHandler(err, 'Erro ao listar produtos'))
+    );
   }
 
   readById(id: string): Observable<Product> {
-    return this.http.get<Product>(`${this.apiURL}/${id}`);
+    return this.http.get<Product>(`${this.apiURL}/${id}`).pipe(
+      map((obj) => obj),
+      catchError((err) => this.errorHandler(err, 'Erro ao buscar produto'))
+    );
   }
 
   update(product: Product): Observable<Product> {
-    return this.http.put<Product>(`${this.apiURL}/${product.id}`, product);
+    return this.http.put<Product>(`${this.apiURL}/${product.id}`, product).pipe(
+      map((obj) => obj),
+      catchError((err) => this.errorHandler(err, 'Erro ao atualizar produto'))
+    );
   }
 
   delete(id: string): Observable<Product> {
-    return this.http.delete<Product>(`${this.apiURL}/${id}`);
+    return this.http.delete<Product>(`${this.apiURL}/${id}`).pipe(
+      map((obj) => obj),
+      catchError((err) => this.errorHandler(err, 'Erro ao excluir produto'))
+    );
   }
 }

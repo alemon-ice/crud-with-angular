e8bcd70bc7805b553c7dbadf5480c81bafad6d2d
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2022-02-02 19:42:06 -0300

    completed

--- src/app/components/template/header/header.component.ts
+++ src/app/components/template/header/header.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderService } from './header.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private headerSerive: HeaderService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  get title(): string {
+    return this.headerSerive.headerData.title;
   }
 
+  get icon(): string {
+    return this.headerSerive.headerData.icon;
+  }
+
+  get routeUrl(): string {
+    return this.headerSerive.headerData.routeUrl;
+  }
 }
